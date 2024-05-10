447b6a55505c17bb314178ce00423929a19243a2
tran-van-ninh <dinhmanhninhtt@gmail.com>
2024-05-10 09:39:21 +0700

    creat method in array ex2

--- lesson_38/main.js
+++ lesson_38/main.js
@@ -2,6 +2,10 @@
 /**
  * ! 1. Map
  * ! 2. Reduce
+ * ! 3. every
+ * ! 4. some
+ * ! 5. find
+ * ! 6. filter
  */
 
 var myArray = [1, 2, 3, 4, 5, 6];
@@ -59,6 +63,7 @@ function creatMethodEveryInArray(arr){
             if(!callBack(item , i, this)){
                 return false;
             }
+            i++;
         }
         return true;
     }
@@ -69,3 +74,63 @@ function creatMethodEveryInArray(arr){
 }
 creatMethodEveryInArray(myArray);
 
+function creatMethodSomeInArray(arr){
+    Array.prototype.some2 = function(callBack){
+        var i = 0;
+        for(var item of this){
+            if(callBack(item, i , this)){
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+    const result = arr.some2(function(item){
+        return item > 5;
+    })
+    
+    console.log(result);
+}
+
+creatMethodSomeInArray(myArray);
+
+function creatMethodFindInArray(arr){
+    Array.prototype.find2 = function (callBack){
+        var i = 0;
+        for(item of this){
+            if(callBack(item , i, this)){
+                return item;
+            }
+            i++;
+        }
+        return undefined;
+    }
+
+    const result = arr.find2(function(item, i){
+        console.log(i,item>2)
+        return item > 2;
+    })
+    console.log(result);
+}
+creatMethodFindInArray(myArray);
+
+function creatMethodFilterInArray(arr){
+    Array.prototype.filter2 = function(callBack){
+        var array = [];
+        var i = 0
+        for(var item of this){
+            if(callBack(item , i, this)){
+                array.push(item);
+            }
+            i++;
+        }
+        return array;
+    }
+
+    const result = arr.filter2(function(item, i){
+        console.log(i, item % 2 === 0)
+        return item % 2 === 0;
+    });
+    console.log(result);
+}
+creatMethodFilterInArray(myArray);
