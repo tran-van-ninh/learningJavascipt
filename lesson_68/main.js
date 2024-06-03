201c9d9fcfe57cef9c51e2c0e4a16786da2be5a6
tran-van-ninh <dinhmanhninhtt@gmail.com>
2024-06-03 10:22:21 +0700

    build tab UI

--- lesson_68/main.js
+++ lesson_68/main.js
@@ -4,10 +4,10 @@ var c = a + b;
 (a + b) > 8 ? console.log(c) : console.log('chúc bạn may mắn lần sau')
 
 const obj = {
-        myFunction: function(message) {
-            //console.log(message)
-            return message
-        }
+        // myFunction: function(message) {
+        //     //console.log(message)
+        //     return message
+        // }
     }
 console.log(obj.myFunction?.("Chúc bạn may mắn lần sau") ? "Chúc bạn may mắn" : "Hẹn gặp lại")
 console.log(obj.myFunction?.(123) ? 'Exist' : 'Not Exist');
